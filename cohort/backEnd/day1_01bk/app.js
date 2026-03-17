c861941ea273601273cf3ca468a2c1e3b21113f7
PUSHPENDRAYDV14 <palyadav1405@gmail.com>
2026-03-17 14:32:38 +0530

    backend folder create an day1bk folder created an work on it

---
+++ cohort/backEnd/day1_01bk/app.js
@@ -0,0 +1,4 @@
+const express = require("express");
+
+const app = express(); //server is created only create kiya hai chalaya nhi hai
+app.listen(3000); //server ko start kerta hai or ye 3000 jo pai ye port number hai is port per start hua hai
