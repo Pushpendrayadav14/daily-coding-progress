348e4a2a187e6516b9ca321e74d508f842cde13e
PUSHPENDRAYDV14 <palyadav1405@gmail.com>
2026-08-07 18:45:10 +0530

    day 11 frontendday11 folder work on reel project form section

--- cohort/backEnd/day11bk/frontendday11/src/App.jsx
+++ cohort/backEnd/day11bk/frontendday11/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { RouterProvider } from "react-router";
 import Approutes from "./Approutes";
+import "./style.scss"
 
 const App = () => {
   return <Approutes />;
--- cohort/backEnd/day11bk/frontendday11/src/features/auth/pages/LoginForm.jsx
+++ cohort/backEnd/day11bk/frontendday11/src/features/auth/pages/LoginForm.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import "../style/form.scss";
 
 const LoginForm = () => {
-  return <div>LoginForm</div>;
+  return (
+    <main>
+      <div className="form-container">
+        <h1>Login</h1>
+        <form>
+          <input
+            type="text"
+            name="username"
+            placeholder="enter your usernmae"
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="enter your password"
+          />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </main>
+  );
 };
 
 export default LoginForm;
