f0df3b0d45bf9ea2b1fdce2169dbaa31b542a928
PUSHPENDRAYDV14 <palyadav1405@gmail.com>
2026-08-20 22:04:47 +0530

    day12bk

---
+++ cohort/frentend/Javascript/intvday01/app.js
@@ -0,0 +1,41 @@
+console.log("javaScript interview prep....");
+// // console.log(5 == "5");
+// // console.log(5 === "5");
+
+// // console.log(name);
+// // var name = "Rahul";
+
+// // sayHello();
+
+// // function sayHello() {
+// //     console.log("Hello");
+// // }
+
+// let arr = [1, 2, 3, 4, 5];
+// // const result = arr.map((num) => num * 2);
+// // console.log(result);
+
+// // const flt = result.filter((num) => num % 4 === 0);
+// // console.log(flt);
+
+// // const rd = flt.reduce((sum, num) => sum + num, 0);
+// // console.log(rd);
+
+// const brr = [...arr, 2, 4, 5, 6, 7];
+// console.log(brr);
+
+// const user = {
+//   name: "Rahul",
+//   age: 22,
+// };
+
+// const { name, age } = user;
+// console.log(name);
+// console.log(age);
+
+window.addEventListener(
+    "scroll",
+    throttle(() => {
+        console.log("Scrolling...");
+    }, 200)
+);
