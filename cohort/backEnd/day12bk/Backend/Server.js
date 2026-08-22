d8f59f3b4f188571cbddf8933c69f5a4de0d21ea
PUSHPENDRAYDV14 <palyadav1405@gmail.com>
2026-08-22 15:24:30 +0530

    remove day12

--- cohort/backEnd/day12bk/Backend/Server.js
+++
@@ -1,7 +0,0 @@
-const app = require("./src/app");
-const connectToDB = require("./src/config/dataBase");
-
-connectToDB();
-app.listen(3000, () => {
-  console.timeLog("server is running on the port 3000...");
-});
